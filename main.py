5772e6538baaacc139fd8f15127ec0c4151b9c14
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-02-06 23:50:12 +0600

    guessing the whole word was added

--- main.py
+++ main.py
@@ -15,24 +15,31 @@ while True:
     print()
 
     letter = input("Letter: ")
-    if letter in ans:
-        print("You have already guessed that letter.")
-    elif letter in word:
-        print("Nice.")
-        for i in range(len(word)):
-            if letter == word[i]:
-                past = ""
-                if i + 1 < len(word):
-                    past = ans[i+1:]
-                ans = ans[:i] + letter + past
+    if not letter:
+        print("You must type letter.")
     else:
-        print("SON OF A BITCH!!!")
+        if len(letter) == 1:
+            if letter in ans:
+                print("You have already guessed that letter.")
+            elif letter in word:
+                print("Nice.")
+                for i in range(len(word)):
+                    if letter == word[i]:
+                        past = ""
+                        if i + 1 < len(word):
+                            past = ans[i+1:]
+                        ans = ans[:i] + letter + past
+            else:
+                print("SON OF A BITCH!!!")
+        else:
+            if letter == word:
+                print("You won! The hidden word : " + word)
+                break
 
-    if ans == word:
-        print("You won! The hidden word : " + word)
-        break
-    if used_attempts == attempts:
-        print("You lost! The hidden word : " + word)
-        break
-
-    used_attempts += 1
+        if ans == word:
+            print("You won! The hidden word : " + word)
+            break
+        if used_attempts == attempts:
+            print("You lost! The hidden word : " + word)
+            break
+        used_attempts += 1
