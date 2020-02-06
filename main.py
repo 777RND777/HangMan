a5760c736b322ebd48f841151f138b585a0b3232
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-02-07 00:08:55 +0600

    output was moved to funcs

--- main.py
+++ main.py
@@ -1,45 +1,21 @@
+from outputs import *
+
 word = "suspend"
 attempts = len(word)
 used_attempts = 0
-ans = ""
+answer = ""
 for i in range(len(word)):
-    ans += "_"
+    answer += "_"
 
 
 while True:
-    print("______________________")
-    print("Attempts:", used_attempts, "/", attempts)
-    print("Current:", end=" ")
-    for i in ans:
-        print(i, end=" ")
-    print()
-
+    show_status(answer, used_attempts, attempts)
     letter = input("Letter: ")
     if not letter:
         print("You must type letter.")
     else:
         if len(letter) == 1:
-            if letter in ans:
-                print("You have already guessed that letter.")
-            elif letter in word:
-                print("Nice.")
-                for i in range(len(word)):
-                    if letter == word[i]:
-                        past = ""
-                        if i + 1 < len(word):
-                            past = ans[i+1:]
-                        ans = ans[:i] + letter + past
-            else:
-                print("SON OF A BITCH!!!")
-        else:
-            if letter == word:
-                print("You won! The hidden word : " + word)
-                break
-
-        if ans == word:
-            print("You won! The hidden word : " + word)
-            break
-        if used_attempts == attempts:
-            print("You lost! The hidden word : " + word)
+            answer = letter_in_answer(letter, answer, word)
+        if is_end(letter, answer, word, used_attempts, attempts):
             break
         used_attempts += 1
---
+++ outputs.py
@@ -0,0 +1,37 @@
+def show_status(ans, used, amount):
+    print("______________________")
+    print("Attempts:", used, "/", amount)
+    print("Current:", end=" ")
+    for i in ans:
+        print(i, end=" ")
+    print()
+
+
+def letter_in_answer(letter, answer, word):
+    if letter in answer:
+        print("You have already guessed that letter.")
+    elif letter in word:
+        print("Nice.")
+        for i in range(len(word)):
+            if letter == word[i]:
+                past = ""
+                if i + 1 < len(word):
+                    past = answer[i + 1:]
+                answer = answer[:i] + letter + past
+    else:
+        print("SON OF A BITCH!!!")
+
+    return answer
+
+
+def is_end(letter, answer, word, used, amount):
+    if letter == word:
+        print("You won! The hidden word : " + word + ".")
+        return True
+    if answer == word:
+        print("You won! The hidden word : " + word + ".")
+        return True
+    if used == amount:
+        print("You lost! The hidden word : " + word + ".")
+        return True
+    return False
